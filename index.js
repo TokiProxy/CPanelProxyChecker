c08beddd27f4c6e2896f34c40d77de3a01bb031c
toki0179 <olliwes01@gmail.com>
2022-02-20 14:40:02 +0000

    Removed console logs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
-const token = "Token Here"
+const token = "Token here"
 
 const config = {
     headers: { Authorization: `Bearer ${token}` }
@@ -24,17 +24,13 @@ axios.get('https://proxy.olliwes.me/api/proxies/socks5', config).then(response =
         let port = ip[1];
         ip = ip[0];
         axios.get(`https://verify.cpanel.net/api/verifyfeed?json=1&ip=${ip}`).then(response => {
-            console.log(response.data)
-
             if (!response.data.license[0].attributes && !response.data.license[0].history) {
                 fs.appendFile('data.txt', `${ip}:${port}\n`, (err) => {
                     if (err) throw err;
-                    console.log('File created');
                 });
             } else if (response.data.license[0] === 0) {
                 fs.appendFile('data.txt', `${ip}:${port}\n`, (err) => {
                     if (err) throw err;
-                    console.log('File created');
                 });
             } else {
                 return;
